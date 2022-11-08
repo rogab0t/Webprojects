7d1af5d59bb2f33bd8f33c6e7d0825bea7bb47ee
Grauu99 <grau.gau99@gmail.com>
2022-11-08 10:07:27 -0600

    Don't repeat yourself/translate to spanish

--- calculadora/codigo.js
+++ calculadora/codigo.js
@@ -1,135 +1,108 @@
-var ruta = window.location;
-console.log(ruta)
-document.write("You are in: " + ruta);
+var elegido = ""
 
-function calcular() 
-{
-  var operacion = prompt("¿Que operación utilizarás?\nsuma, resta, multiplicacion o division:");
-  switch(operacion) {
-    case "suma":
-    case "+":
-      function changeSum() {
-        var change = prompt("Elegiste SUMA.\n¿Quieres cambiar de operación?\nsi o no")
-        if(change == "si") {
-          calcular() 
-        } else if(change == "no") {
-            suma()
-        } else {
-            alert("ingresa una respuesta valida")
-            changeSum()
-        }
-      }
-      changeSum()
-      
-      function suma()
-      {
-        var first_number = parseFloat(prompt("ingresa el primer número"));
-        var second_number = parseFloat(prompt("ingresa el segundo número"));
-        var invalid_number = ("No se ingresaron valores válidos");
-        if(isNaN(first_number && second_number)) {
-          alert(invalid_number);
-          suma();
-        } else {
-            var resultado = (first_number + second_number);
-            alert("El resultado de la suma es " + resultado);
-        }
-      }
-    break;
-  
-    case "resta":
-    case "-":
-        function changeRes() {
-          var change = prompt("Elegiste RESTA.\n¿Quieres cambiar de operación?\nsi o no")
-          if(change == "si") {
-            calcular() 
-          } else if(change == "no") {
-              resta()
-          } else {
-              alert("ingresa una respuesta valida")
-              changeRes()
-          }
-        }
-        changeRes()
+function changeOperation(operation) {
+    var change = prompt("Elegiste: " + elegido + "\n¿Quieres cambiar de operación?\nsi o no")
+    if(change == "si") {
+      calcular() 
+    } else if(change == "no") {
+        operation()
+    } else {
+        alert("ingresa una respuesta valida")
+        changeOperation(operation)
+    }
+}
+
+function calcular() {
+    var operacion = prompt("¿Que operación utilizarás?\nsuma: + \nresta: - \nmultiplicacion: *  \ndivision: /");
+    switch(operacion) {
+        case "suma":
+        case "+":
+            var operacionElegida = "suma"
+            elegido = operacionElegida
+
+            function suma(){
+                var first_number = parseFloat(prompt("ingresa el primer número"));
+                var second_number = parseFloat(prompt("ingresa el segundo número"));
+                var invalid_number = ("No se ingresaron valores válidos");
+                if(isNaN(first_number && second_number)) {
+                    alert(invalid_number);
+                    suma();
+                } else {
+                    var resultado = (first_number + second_number);
+                    alert("El resultado de la suma es: " + resultado);
+                }
+            }
+            changeOperation(suma)
+        break;
     
-      function resta() {
-        var first_number = parseFloat(prompt("ingresa el primer número"));
-        var second_number = parseFloat(prompt("ingresa el segundo número"));
-        var invalid_number = ("No se ingresaron valores válidos");
-        if(isNaN(first_number && second_number)) {
-          alert(invalid_number);
-          resta();
-        } else {
-            var resultado = (first_number - second_number);
-            alert("El resultado de la resta es " + resultado);
-        }
-      }
-    break;
-  
-    case "multiplicacion":
-    case "*":
-        function changeMul() {
-          var change = prompt("Elegiste MULTIPLICACIÓN.\n¿Quieres cambiar de operación?\nsi o no")
-          if(change == "si") {
-            calcular() 
-          } else if(change == "no") {
-              multi()
-          } else {
-              alert("ingresa una respuesta valida")
-              changeMul()
-          }
-        }
-        changeMul()
+        case "resta":
+        case "-":
+            var operacionElegida = "resta"
+            elegido = operacionElegida
+        
+            function resta() {
+                var first_number = parseFloat(prompt("ingresa el primer número"));
+                var second_number = parseFloat(prompt("ingresa el segundo número"));
+                var invalid_number = ("No se ingresaron valores válidos");
+                if(isNaN(first_number && second_number)) {
+                    alert(invalid_number);
+                    resta();
+                } else {
+                    var resultado = (first_number - second_number);
+                    alert("El resultado de la resta es: " + resultado);
+                }
+            }
+            changeOperation(resta)
+        break;
     
-      function multi() {
-        var first_number = parseFloat(prompt("ingresa el primer número"));
-        var second_number = parseFloat(prompt("ingresa el segundo número"));
-        var invalid_number = ("No se ingresaron valores válidos");
-        if(isNaN(first_number && second_number)) {
-          alert(invalid_number);
-          multi();
-        } else {
-            var resultado = (first_number * second_number);
-            alert("El resultado de la multiplicación es " + resultado);
-        } 
-      }
-      break;
-  
-    case "division":
-    case "/":
-        function changeDiv() {
-          var change = prompt("Elegiste DIVISÓN.\n¿Quieres cambiar de operación?\nsi o no")
-          if(change == "si") {
-            calcular() 
-          } else if(change == "no") {
-            div()
-          } else {
-              alert("ingresa una respuesta valida")
-              changeDiv()
-          }
-        }
-        changeDiv()
+        case "multiplicacion":
+        case "*":
+            var operacionElegida = "multiplicacion"
+            elegido = operacionElegida
+        
+            function multi() {
+                var first_number = parseFloat(prompt("ingresa el primer número"));
+                var second_number = parseFloat(prompt("ingresa el segundo número"));
+                var invalid_number = ("No se ingresaron valores válidos");
+                if(isNaN(first_number && second_number)) {
+                    alert(invalid_number);
+                    multi();
+                } else {
+                    var resultado = (first_number * second_number);
+                    alert("El resultado de la multiplicación es: " + resultado);
+                } 
+            }
+            changeOperation(multi)
+        break;
     
-      function div() {
-        var first_number = parseFloat(prompt("ingresa el primer número"));
-        var second_number = parseFloat(prompt("ingresa el segundo número"));
-        var invalid_number = ("No se ingresaron valores válidos");
-        if(isNaN(first_number && second_number)) {
-          alert(invalid_number);
-          div();
-        } else {
-            var resultado = (first_number / second_number);
-            alert("El resultado de la división es " + resultado);
-        }
-      }
-      break;
-      
-    default:
-      function invalid() {
-        var inv = ("No se ingresó una operación válida");
-        alert(inv);
-        calcular();
-      }
-    invalid();
-  }
+        case "division":
+        case "/":
+            var operacionElegida = "division"
+            elegido = operacionElegida
+        
+            function div() {
+                var first_number = parseFloat(prompt("ingresa el primer número"));
+                var second_number = parseFloat(prompt("ingresa el segundo número"));
+                var invalid_number = ("No se ingresaron valores válidos");
+                if(isNaN(first_number && second_number)) {
+                    alert(invalid_number);
+                    div();
+                } else {
+                    var resultado = (first_number / second_number);
+                    alert("El resultado de la división es: " + resultado);
+                }
+            }
+            changeOperation(div)
+        break;
+        
+        default:
+            function invalid() {
+                var inv = ("No se ingresó una operación válida");
+                alert(inv);
+                calcular();
+            }
+            invalid();
+        break;
+    }
 }
 calcular();
