a99e0420fc121ea2ccdd40354479335eb339c28f
Rodrigo Grau <grau.gau99@gmail.com>
2023-05-02 14:38:06 -0600

    index: chosen for child, css: change styles for a best responsive, js: added two new elements, images: added two new images of childs

--- landingEvangelion/assets/javascript/slider/elements.js
+++ landingEvangelion/assets/javascript/slider/elements.js
@@ -1,21 +1,33 @@
 const elements = [ 
     { 
         title: 'Rei Ayanami', 
-        subtitle: 'First chosen', 
+        subtitle: 'First child', 
         image: './public/images/image-4.webp', 
         text: 'She is a skilled pilot of the EVAs, and her apparent lack of emotions often makes her appear cold and distant. However, as the series progresses, details about her past and her true identity are revealed, making her one of the most interesting and complex characters in Evangelion.' 
     },
     { 
         title: 'Shinji Ikary', 
-        subtitle: 'Second chosen', 
+        subtitle: 'Second child', 
         image: './public/images/image-5.jpeg', 
         text: 'Introverted and emotionally vulnerable, he faces traumas and challenges that force him to confront his fears and find his purpose in life.' 
     },
     { 
         title: 'Asuka Langley', 
-        subtitle: 'Third chosen', 
+        subtitle: 'Third child', 
         image: './public/images/image-6.webp', 
         text: 'Highly competent with a strong and dominant personality. She often comes off as arrogant and self-assured, but she is also vulnerable and struggles to relate to others. Asuka has a complicated relationship with her past and her family, which leads her to grapple with issues of trust and self-esteem.' 
+    },
+    {
+        title: 'Toji Suzuhara', 
+        subtitle: 'Fourth child',
+        image: './public/images/image-7.png', 
+        text: 'Toji is Shinji´s friend, his life changes dramatically when his younger sister Hikari is severely injured during an attack by one of the Angels and he blames Shinji, but eventually understands the situation and reconciles with his friend. Later, Toji is selected as the fourth Evangelion pilot. During the battle with the thirteenth Angel, Toji and his unit are severely injured.' 
+    },
+    {
+        title: 'kaworu nagisa', 
+        subtitle: 'Fifth child', 
+        image: './public/images/image-8.jpeg', 
+        text: 'Sent by the SEELE organization to NERV, Kaworu Nagisa is a kind, gentle, and empathetic character who quickly gains the sympathy of other characters, especially Shinji. Kaworu takes a keen interest in Shinji, and the two develop a close relationship in a short amount of time. Kaworu´s true mission is to evaluate humanity and determine if it is necessary to proceed with the Human Instrumentality Project.' 
     }
 ];
 
